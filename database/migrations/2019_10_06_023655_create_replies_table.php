ea1848ed779672fc3ab49591ee56afcd6617ebc3
root <root@cafe153220.tw>
2019-10-06 11:08:39 +0800

    create Migrations, Model, Controllers

--- database/migrations/2019_10_06_023655_create_replies_table.php
+++ database/migrations/2019_10_06_023655_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionsTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,13 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->text('body');
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id');
+            $table->foreign('question_id')->references('id')->on('questions')
+            ->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +31,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('replies');
     }
 }
