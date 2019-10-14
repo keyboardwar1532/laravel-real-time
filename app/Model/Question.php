be8b0f9bce2f95cc9b4628bd12b6acb18d0471a2
root <root@cafe153220.tw>
2019-10-14 21:12:21 +0800

    add relations

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -1,10 +1,21 @@
 <?php
 
 namespace App\Model;
+use App\User;
+// use App\Model\Reply;
+// use App\Model\Category;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+    public function category(){
+        return $this->belongs(Category::class);
+    }
 }
